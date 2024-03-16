8b7f37bd346e0bde9cf3e8f7a2e4bc1af22770fb
Casper Pulit <pulitcasper@gmail.com>
2024-03-16 14:30:05 +0000

    some more loops stuff, variatic functions and multi return functions

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Define function (no return)
 func odd_or_even(n int) {
 	if n%2 == 0 {
 		fmt.Println(n, "is even")
@@ -22,14 +23,59 @@ func fizzBuzz(n int) {
 	}
 }
 
+// func with return
+// func func_name(arg arg_type, ...) return_type {...}
+func sum(a int, b int, c int) int {
+	return (a + b + c)
+}
+
+// func multiple return values
+// func func_name(arg arg_type, ...) (return_type_1, return_type_2) {...}
+func vals() (int, int) {
+	return 3, 7
+}
+
+// variadic functions
+// can take any number of arugments
+// e.g. fmt.Println
+
+// func func_name(arg ... arg_type, ...) return_type {...}
+func betterSum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
 func main() {
 
 	for i := 1; i <= 10; i++ {
 		odd_or_even(i)
 	}
 
-	for i := 1; i <= 100000; i++ {
+	for i := 1; i <= 100; i++ {
 		fizzBuzz(i)
 	}
 
+	var out = sum(1, 2, 3)
+
+	fmt.Println(out)
+
+	fmt.Println("\n\n\n\n")
+
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+	// blank identifer "_" to get subset of values
+	_, c := vals()
+	fmt.Println(c)
+
+	fmt.Println("\n\n\n\n")
+
+	betterSum(1, 2, 3)
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	betterSum(nums...)
 }
