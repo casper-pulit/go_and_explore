c27ccdf41221dcb0912b49daea6f0f45be310f52
casper-pulit <pulitcasper@gmail.com>
2024-03-16 00:23:59 +0000

    continue exploring

--- main.go
+++ main.go
@@ -43,4 +43,53 @@ func main() {
 
 	// bools don't support mathematical operations
 
+	// Vars
+
+	// defined using var
+	// var varname = value
+	// var varname1, varname2 = value1, value2
+
+	var a = 1
+	var b, c = 2, 3
+
+	fmt.Println("a + b + c =", a+b+c)
+
+	// vars can be typed
+
+	var d int = 2000
+	var e bool = true
+	var f string = "this is a string"
+
+	fmt.Println("vars d, e and f:", d, e, f)
+
+	// what happens when assigning a new type to exisiting var
+	// compiler error
+
+	// declaring a variable
+	var declared_int int
+	var declared_float32 float32
+	var declared_float64 float64
+	var declared_string string
+	var declared_bool bool
+
+	fmt.Println(declared_int) // int == 0
+
+	fmt.Println(declared_float32) // float32 == 0
+
+	fmt.Println(declared_float64) // float64 == 0
+
+	fmt.Println(declared_string) // string == ""
+	fmt.Println(declared_string == "")
+
+	fmt.Println(declared_bool) // bool == false
+
+	// declaring and initliasing variable
+
+	walrus := "they are cute"
+	fmt.Println(walrus)
+
+	// consts (revist later)
+
+	// for (very exciting)
+
 }
