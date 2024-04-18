0edd5a340057742be9f092e7de7460050350225e
casper-pulit <pulitcasper@gmail.com>
2024-04-18 22:23:59 +0100

    working main huzzah

--- cmd/goAndExplore/main.go
+++ cmd/goAndExplore/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/casper-pulit/goAndExplore/internal/explore"
 )
 
 func main() {
 	fmt.Println("Main!")
-	fmt.Println(explore.helloWorld())
+	fmt.Println(explore.HelloWorld())
 }
